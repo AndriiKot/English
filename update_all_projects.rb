3c454fc92c5dda6e2d0fabdb035eda1cd6df5638
AndriiKot <a_kocyuba@ukr.net>
2024-05-30 11:17:14 +0200

    ENGLISH  !!!! TESTING 'update_all_projects.rb'  2 !!!!

--- update_all_projects.rb
+++ update_all_projects.rb
@@ -1,6 +1,4 @@
-﻿number_of_lessons = (1..25)
-max_value_size = number_of_lessons.max.to_s.size
-
+﻿max_value_size = 25
 
 repositories = [
     'https://github.com/AndriiKot/Lesson_1__Course_25__A0-A1__.git',
@@ -11,8 +9,8 @@ update = "git subtree pull --prefix "
 
 repositories.each_with_index do |element, index|
     folder = "_#{'0' * (max_value_size - (index + 1).to_s.size)}#{index + 1}__Lesson__"
+    git_repo = element
         system(`#{update}#{folder} #{git_repo} main`)
-
 end
 
 system(`git push`)
