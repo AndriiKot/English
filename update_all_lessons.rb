b6d8dea29d05e9ca5821bc60fe2bd0fb1c526156
AndriiKot <a_kocyuba@ukr.net>
2024-06-08 14:33:31 +0200

    ENGLISH  !!! UDATE testing !!!

---
+++ update_all_lessons.rb
@@ -0,0 +1,11 @@
+﻿projects = {
+  "Marathon_English_UA": "https://github.com/AndriiKot/Marathon__English__UA.git",
+}
+
+update = "git subtree pull --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
