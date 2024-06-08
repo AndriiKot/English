13abe1c5cc36a295783ffe65711b3d03e5a3d869
AndriiKot <a_kocyuba@ukr.net>
2024-06-08 13:50:55 +0200

    ENGLISH  Marathon_UA !!! create updata_all_projects.rb !!!

---
+++ update_all_projects.rb
@@ -0,0 +1,11 @@
+﻿projects = {
+  _0_day: "https://github.com/AndriiKot/Marathon__acquaintance__day_1.git",
+}
+
+update = "git subtree pull --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
