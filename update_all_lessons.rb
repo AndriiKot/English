0d7fd2bac5fedbc4728f340495b1b43aa72ef52f
AndriiKot <a_kocyuba@ukr.net>
2024-06-12 10:20:28 +0200

    ENGLISH  Marathon_UA !!! ADDED new lesson UPDATE update_all_lessons.rb !!!

--- update_all_lessons.rb
+++ update_all_lessons.rb
@@ -2,6 +2,7 @@
   "_0-1_day": "https://github.com/AndriiKot/Marathon__acquaintance__day_1.git",
   "_0-2_day": "https://github.com/AndriiKot/Marathon__acquaintance__day_2.git",
   "_1_day": "https://github.com/AndriiKot/PresentSimple_Count_PreInter_Marathon_UA.git",
+  "_2_day": "https://github.com/AndriiKot/Past_Simple__Marathon__UA.git",
 }
 
 update = "git subtree pull --prefix "
