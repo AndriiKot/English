cf590f907917ba0dfd9c7e58c620ffd39bf136ae
AndriiKot <a_kocyuba@ukr.net>
2024-06-16 00:19:57 +0200

    ENGLISH  Marathon_UA  !! update update_all_lessons.rb !!

--- update_all_lessons.rb
+++ update_all_lessons.rb
@@ -1,6 +1,6 @@
 ﻿projects = {
-  "_0-1_day": "https://github.com/AndriiKot/Marathon__acquaintance__day_1.git",
-  "_0-2_day": "https://github.com/AndriiKot/Marathon__acquaintance__day_2.git",
+  "_0-1_day": "https://github.com/AndriiKot/Marathon__Acquaintance__day_1.git",
+  "_0-2_day": "https://github.com/AndriiKot/Marathon__Acquaintance__day_2.git",
   "_1_day": "https://github.com/AndriiKot/PresentSimple_Count_PreInter_Marathon_UA.git",
   "_2_day": "https://github.com/AndriiKot/Past_Simple__Marathon__UA.git",
   "_3_day": "https://github.com/AndriiKot/Future_Simple__Marathon__UA.git",
